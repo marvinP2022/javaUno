3acb69745d72ecf4e52c6c324552ddb676958df3
marvinP2022 <marvin.parrales@fcys.uni.edu.ni>
2022-02-23 11:52:31 -0600

    Tercero

---
+++ src/proyectogit/NewClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectogit;
+
+/**
+ *
+ * @author JADPA13
+ */
+public class NewClass {
+    int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+    
+    
+}
